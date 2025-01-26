68bfbb1ec10b04ffdb0775a4f432df43a20e7abf
arjunsingh4545 <115133870+arjunsingh4545@users.noreply.github.com>
2025-01-26 19:33:40 +0530

    Time: 83 ms (94.17%) | Memory: 8.9 MB (29.76%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,15 +1,27 @@
 class Solution {
-    bool solve(vector<vector<char>>& board) {
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                if (board[i][j] == '.') { // Find an empty cell
-                    for (char c = '1'; c <= '9'; ++c) { // Try digits 1-9
-                        if (isValid(board, i, j, c)) {
-                            board[i][j] = c; // Place the digit
-                            if (solve(board)) { // Recursively try to solve
+     bool solve(vector<vector<char>>& board, array<array<bool, 9>, 9>& rows,
+               array<array<bool, 9>, 9>& cols, array<array<bool, 9>, 9>& boxes) {
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] == '.') { // Find an empty cell
+                    for (int num = 0; num < 9; ++num) { // Try digits 1-9
+                        if (!rows[r][num] && !cols[c][num] && !boxes[(r / 3) * 3 + (c / 3)][num]) {
+                            // Place the digit
+                            board[r][c] = num + '1'; // Convert int to char
+                            rows[r][num] = true;
+                            cols[c][num] = true;
+                            boxes[(r / 3) * 3 + (c / 3)][num] = true;
+
+                            // Recursively try to solve
+                            if (solve(board, rows, cols, boxes)) {
                                 return true;
                             }
-                            board[i][j] = '.'; // Backtrack
+
+                            // Backtrack
+                            board[r][c] = '.';
+                            rows[r][num] = false;
+                            cols[c][num] = false;
+                            boxes[(r / 3) * 3 + (c / 3)][num] = false;
                         }
                     }
                     return false; // No valid number found, need to backtrack
@@ -18,27 +30,27 @@ class Solution {
         }
         return true; // Solved
     }
-    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
-        // Check the row and column
-        for (int i = 0; i < 9; ++i) {
-            if (board[row][i] == c || board[i][col] == c) {
-                return false;
-            }
-        }
-        // Check the 3x3 sub-box
-        int boxRowStart = (row / 3) * 3;
-        int boxColStart = (col / 3) * 3;
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                if (board[boxRowStart + i][boxColStart + j] == c) {
-                    return false;
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        // Initialize the tracking arrays
+        array<array<bool, 9>, 9> rows = {};
+        array<array<bool, 9>, 9> cols = {};
+        array<array<bool, 9>, 9> boxes = {};
+
+        // Fill the tracking arrays with existing numbers
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] != '.') {
+                    int num = board[r][c] - '1'; // Convert char to int (0-8)
+                    rows[r][num] = true;
+                    cols[c][num] = true;
+                    boxes[(r / 3) * 3 + (c / 3)][num] = true;
                 }
             }
         }
-        return true; // Valid placement
-    }
-public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+
+        // Start solving the Sudoku
+        solve(board, rows, cols, boxes);
+
     }
 };
