1b9c8abfc37c11a365e1073c0239a0bd04450c94
arjunsingh4545 <115133870+arjunsingh4545@users.noreply.github.com>
2025-04-23 23:06:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (91.99%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,19 +1,22 @@
 class Solution {
-    int dfs(string& s , int i , unordered_map<int , int>& dp) {
-        if(dp.count(i)) return dp[i];
-        if(s[i] == '0') return 0;
-        int res = dfs(s , i+1 , dp); 
-        if (i + 1 < s.size() && (s[i] == '1' || 
-            s[i] == '2' && s[i + 1] < '7')) {
-            res += dfs(s, i + 2, dp);
-        }
-        dp[i] = res;
-        return res;
-    }
 public:
     int numDecodings(string s) {
-        unordered_map<int,int> dp;
-        dp[s.size()]  = 1;
-        return dfs(s , 0 , dp);
+        int dp = 0, dp2 = 0;
+        int dp1 = 1;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] == '0') {
+                dp = 0;
+            } else {
+                dp = dp1;
+                if (i + 1 < s.size() && (s[i] == '1' || 
+                    s[i] == '2' && s[i + 1] < '7')) {
+                    dp += dp2;
+                }
+            }
+            dp2 = dp1;
+            dp1 = dp;
+            dp = 0;
+        }
+        return dp1;
     }
 };
